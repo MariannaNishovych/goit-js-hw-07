d27a29fa8c4d0eec5a72db2e7bd089a323b7b3da
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-08-19 16:31:48 +0300

    add task-3

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const textInput = document.querySelector('#name-input');
+
+const nameOutput = document.querySelector('#name-output');
+
+textInput.addEventListener('input', (event) => {
+nameOutput.textContent = event.currentTarget.value.trim() || 'Anonymous';
+});
